7a12af9d45de533b16dca6c929ea0592c5562071
Alin Lupascu <lupascu.marius@gmail.com>
2023-07-27 12:25:09 -0600

    Update sdm.rb

--- Casks/sdm.rb
+++ Casks/sdm.rb
@@ -1,6 +1,6 @@
 cask "sdm" do
   version "20.67.0,25CD2145AECBEAD9F2A128819B43CD1D1585AC87"
-  sha256 "75fa6b739c696bec8aa9b52cb96b0797e8bd37c85b85623c05fd6bde16d49e12"
+  sha256 "66805f4163ee014dd981503cc166d571aaf34d5422d01dde129f532665fe1420"
 
   url "https://downloads.strongdm.com/builds/desktop/#{version.csv.first}/darwin/universal/#{version.csv.second}/SDM-#{version.csv.first}.universal.zip"
   name "sdm"

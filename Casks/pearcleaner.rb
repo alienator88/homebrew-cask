df7f2db19596bbbcde2f183facc2de7528ecb9f1
Alin Lupascu <lupascu.marius@gmail.com>
2023-11-15 10:33:44 -0700

    Update pearcleaner.rb

--- Casks/pearcleaner.rb
+++ Casks/pearcleaner.rb
@@ -2,7 +2,7 @@ cask "pearcleaner" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/alienator88/Pearcleaner/releases/download/latest/Pearcleaner.zip"
+  url "https://github.com/alienator88/Pearcleaner"
   name "Pearcleaner"
   desc "Utility to uninstall apps and clean-up leftover files"
   homepage "https://github.com/alienator88/Pearcleaner/"

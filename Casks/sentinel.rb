54f8af36b32351382e204ae0eadf5e0f2c42d5e6
Alin Lupascu <lupascu.marius@gmail.com>
2024-03-26 18:20:56 -0600

    Create sentinel.rb

---
+++ Casks/sentinel.rb
@@ -0,0 +1,20 @@
+cask "sentinel" do
+  version "1.3"
+  sha256 :no_check
+
+  # Version
+  url "https://github.com/alienator88/Sentinel/releases/download/#{version}/Sentinel.zip"
+  name "Sentinel"
+  desc "Utility to control Gatekeeper via a GUI"
+  homepage "https://github.com/alienator88/Sentinel"
+
+  depends_on macos: ">= :monterey"
+
+  app "Pearcleaner.app"
+
+  uninstall quit:      "com.alienator88.sentinel"
+  
+  zap trash: [
+    "~/Library/Application Support/com.alienator88.sentinel",
+  ]
+end

d91716c734ec4809e03cb432a467347bac66980e
Alin Lupascu <lupascu.marius@gmail.com>
2024-04-08 17:08:31 -0600

    Create viz.rb

---
+++ Casks/viz.rb
@@ -0,0 +1,20 @@
+cask "viz" do
+  version "1.0"
+  sha256 :no_check
+
+  # Version
+  url "https://github.com/alienator88/Viz/releases/download/#{version}/Viz.zip"
+  name "Viz"
+  desc "Utility to extract text from images, videos, qr/barcodes"
+  homepage "https://github.com/alienator88/Viz"
+
+  depends_on macos: ">= :monterey"
+
+  app "Viz.app"
+
+  uninstall quit:      "com.alienator88.Viz"
+  
+  zap trash: [
+    "~/Library/Application Support/com.alienator88.Viz",
+  ]
+end
